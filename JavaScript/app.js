8fad6ac252a99e8edf60e8cc49cc4a53e51cf92d
Rodrigo Caraduje <yoyi_rodri98@hotmail.com>
2021-10-04 19:35:12 -0300

    Objetos

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -36,7 +36,7 @@ while(contraseña != "1234" ){
 
 
 /* Simulador interactivo Tp + Funciones relacionadasTp*/
-const iva = 21
+/* const iva = 21
 const papasFritas = 350;
 const palitosFritos = 260;
 const maniSaborizado = 527;
@@ -64,4 +64,54 @@ const procesoIva = () =>{
     let productoElegido = defineProducto(dato);
     return productoElegido + compraIva(productoElegido)
 }
-console.log(procesoIva());
+console.log(procesoIva()); */
+
+
+/* Desafio de objetos*/
+
+class productos{
+    constructor(nombre,precio,peso,stock){
+        this.nombre = nombre,
+        this.precio = precio,
+        this.peso = peso,
+        this.stock = stock
+    }
+
+    getNombre = () => {return this.nombre}
+    getPrecio = () => {return this.precio}
+    getPeso = () => {return this.peso}
+    getStock = () => {return this.stock > 0 ? true : false}
+}
+
+const chicito = new productos ("Julicroc", 300, 500, 0);
+const palitosFritos = new productos ("Laurens", 250, 1000, 15);
+const papasFritas = new productos ("Lays", 450, 800, 7);
+
+const defineProductos = (elemento) => {
+    switch (elemento){
+        case "Julicroc": 
+                return chicito;
+        case "Laurens":
+                return palitosFritos;
+        case "Lays":
+                return papasFritas; 
+    }
+};
+
+comprar = () => {
+    let product = prompt("Ingrese su Producto")
+    const eligeProducto = defineProductos (product);
+    if(product.getStock()){
+        eligeProducto.stock = eligeProducto.stock - 1;
+        console.log("Compraste un producto");
+        console.log("Veo el envio del producto",eligeProducto);
+    }
+    else{
+        console.log("No hay stock disponible");
+    }
+};
+comprar();
+
+
+
+
