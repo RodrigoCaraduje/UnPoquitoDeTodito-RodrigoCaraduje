29501bfc38350d1467aa6d8eecab67085b9587d6
Rodrigo Caraduje <yoyi_rodri98@hotmail.com>
2021-10-18 21:51:12 -0300

    jq

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,75 +1,6 @@
-/* alert ("Hola mundo, esto es js")
-let nombre = prompt ("Ingrese su nombre")
-let apellido = prompt ("Ingrese su apellido")
-let edad = Number(prompt("Ingrese su edad"))
-let palabra
-let dinero = Number(prompt("ingrese su dinero"))
-let sexo
-
-if (dinero === 160){
-    alert ("Felicidades, te podes comprar 1 dolar xd")
-}else if (dinero > 160){
-    alert ("Wow, con esa cantidad hasta te compras la pampa")
-}else {
-    alert ("Te podes hacer un guisito al menos")
-}
-
-if (sexo === "H"){
-    palabra = "Bienvenido"
-}else if (sexo === "M"){
-    palabra = "Bienvenida"
-}else {
-    palabra = "Quien es ese pokemonnnn"
-}
-
-console.log (`$(palabra) $(nombre) $(apellido) tu edad es $(edad)`)
-
-let contraseña = prompt("Ingrese su contraseña");
-
-while(contraseña != "1234" ){
-    alert("El usuario ingresó "+ contraseña);
-    
-    contraseña = prompt("Reintentar contraseña");
-} */
-
-
-
-
-/* Simulador interactivo Tp + Funciones relacionadasTp*/
-/* const iva = 21
-const papasFritas = 350;
-const palitosFritos = 260;
-const maniSaborizado = 527;
-const aritosFrutales = 1349;
-
-const compraIva = (productoElegido) => {
-    return productoElegido * iva / 100;
-};
-
-const defineProducto = (producto) => {
-    switch (producto){
-        case "papasF":
-                return papasFritas;
-        case "palitosF":
-                return palitosFritos;
-        case "maniS":
-                return maniSaborizado;
-        case "aritosF":
-                return aritosFrutales;
-    }
-};
-
-const procesoIva = () =>{
-    const dato = prompt("Ingrese su producto");
-    let productoElegido = defineProducto(dato);
-    return productoElegido + compraIva(productoElegido)
-}
-console.log(procesoIva()); */
-
-
 /* Desafio de objetos*/
 
-class productos{
+/* class productos{
     constructor(nombre,precio,peso,stock){
         this.nombre = nombre,
         this.precio = precio,
@@ -110,4 +41,18 @@ comprar = () => {
         console.log("No hay stock disponible");
     }
 };
-comprar();
+comprar(); */
+
+/*Tp Arrays */
+
+const listaProductos = ["Arandanos","Nueces"];
+let   cantidad = 10;
+do{
+    let seleccion = prompt("Ingresar producto");
+    listaProductos.push(seleccion.toUpperCase());
+    console.log(listaProductos.length);
+}while(listaProductos.length != cantidad)
+
+const nuevaListaProductos = listaProductos.concat([]);
+
+alert(nuevaListaProductos.join("\n"));
